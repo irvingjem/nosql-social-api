aa91012232dd5edb447c1d25754f293ebd0d03f6
Your Name <irving.jem@gmail.com>
2022-05-23 10:47:35 -0700

    set up mongoose connection

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
-const mongoose = require('mongoose')
+// Node Dependancies
 const express = require('express')
+const mongoose = require('mongoose')
+
+// Server
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 
-await mongoose.connect('mongodb://localhost/my_database');
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-main().catch(err => console.log(err));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+// Connection to mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Log mongoose queries
+mongoose.set('debug', true);
+
+
+app.listen(PORT, () => console.log(` **** Connected on localhost:${PORT} **** `));
