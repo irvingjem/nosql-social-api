4cade153de6092cf630db6d2db0ee98c7d479b73
Your Name <irving.jem@gmail.com>
2022-05-30 08:59:19 -0700

    progress getting routes to work

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,6 @@
 // Require express router
 const router = require('express').Router();
+
+
 // Module export router
 module.exports = router;
--- server.js
+++ server.js
@@ -26,4 +26,4 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-
 mongoose.set('debug', true);
 
 
-app.listen(PORT, () => console.log(` **** Connected on localhost:${PORT} **** `));
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
