7e45929edc79f32773559e942101df1235a0ddf6
Your Name <irving.jem@gmail.com>
2022-05-31 12:48:43 -0700

    making progression on models

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,3 +1,30 @@
 const { Schema, module, Types } = require('mongoose');
 
+// ReactionsSchema
+const ReactionsSchema = new Schema({
+    // Set custom ID 
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+}, {
+    toJSON: {
+        getters: true
+    }
+});
+
 module.exports = Thoughts;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,16 @@
 // Require express router
 const router = require('express').Router();
 
+// Gets from the controllers
+const {
+    getAllUsers,
+    getUsersById,
+    createUsers,
+    updateUsers,
+    deleteUsers,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/users-controller');
 
 // Module export router
 module.exports = router;
