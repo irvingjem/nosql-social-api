b1a623c0db83e67b7a9c86d889e2292da7ef4247
Your Name <irving.jem@gmail.com>
2022-06-12 13:40:06 -0700

    moved mongo connection

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Node Dependancies
 const express = require('express')
-const mongoose = require('mongoose')
+const db = require('./config/connection');
+const routes = require('./routes');
+
 
 // Server
 const app = express();
@@ -11,14 +13,5 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes'));
-
-// Connection to mongoose
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-// Log mongoose queries
-mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
